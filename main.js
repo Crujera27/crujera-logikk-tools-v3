7290a0ecffc352af23024fcd82dc00e3595f18e7
Crujera27 <71277595+Crujera27@users.noreply.github.com>
2023-03-10 16:10:04 +0100

    Loocking good

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const emojis = require('./config/emojis.json')
 const mysql = require('mysql');
 const { Client, Intents } = require('discord.js');
 const client = new Client({ 
@@ -21,11 +22,8 @@ client.on('ready', () => {
   clearOldWarns()
   client.user.setPresence({
     status: 'dnd',
-    activity: {
-      name: "Logikk's Discord",
-      type: 'WATCHING'
-    }
   });
+  client.user.setActivity('Logikk\'s Discord', { type: 'WATCHING' })
 });
 
 client.on('message', message => {
@@ -33,13 +31,13 @@ client.on('message', message => {
     
       
       if (message.content.startsWith('lt!warn')) {
-if (!message.member.permissions.has('MANAGE_ROLES')) {
-  return message.reply('No tienes permiso para ejecutar este comando');
-}
+      if (!message.member.permissions.has('MANAGE_ROLES')) {
+          return;
+        }
         // Obtenemos el usuario que recibirá el warn
         const user = message.mentions.users.first();
         if (!user) {
-          return message.reply('Debes mencionar a un usuario para aplicarle un warn.');
+          return message.reply(':x: | Debes mencionar a un usuario para aplicarle un warn.');
         }
       
         // Obtenemos el grado de warn
@@ -47,7 +45,7 @@ if (!message.member.permissions.has('MANAGE_ROLES')) {
         let warnLevel = warnLevelArg ? warnLevelArg.toLowerCase() : 'leve'; // si no se especifica, se asume leve
       
         if (warnLevel !== 'leve' && warnLevel !== 'medio' && warnLevel !== 'grave') {
-          return message.reply('El grado de warn debe ser leve, medio o grave.');
+          return message.reply('El grado de warn debe ser `leve`, `medio` o `grave`.');
         }
       
         // Obtenemos la razón del warn
@@ -78,7 +76,7 @@ if (!message.member.permissions.has('MANAGE_ROLES')) {
           if (warnLevel === 'leve' && warnCount.leve >= 3) {
             warnLevel = 'medio';
             applyWarn = false;
-            banMessage = `⚠ ${user.username} ha superado el máximo de warns leve. Este debería de ser aislado temporalmente y aplicarle un warn medio de acuerdo con la normativa vigente.`;
+            banMessage = emojis.warn+` | ${user.username} ha superado el máximo de warns leve. Este debería de ser aislado temporalmente y aplicarle un warn medio de acuerdo con la normativa vigente.`;
             db.query(`DELETE FROM warns WHERE user_id = '${user.id}' AND level = 'leve' LIMIT 3`, (err) => {
               if (err) throw err;
               console.log(`Se han eliminado 3 warns leves de ${user.username}.`);
@@ -86,14 +84,14 @@ if (!message.member.permissions.has('MANAGE_ROLES')) {
           } else if (warnLevel === 'medio' && warnCount.medio >= 3) {
             warnLevel = 'grave';
             applyWarn = false;
-            banMessage = `⚠ | ${user.username} ha superado el máximo de warns medios. Este debería de ser aislado temporalmente y aplicarle un warn grave de acuerdo con la normativa vigente.`;
+            banMessage = emojis.warn+` | ${user.username} ha superado el máximo de warns medios. Este debería de ser aislado temporalmente y aplicarle un warn grave de acuerdo con la normativa vigente.`;
             db.query(`DELETE FROM warns WHERE user_id = '${user.id}' AND level = 'medio' LIMIT 3`, (err) => {
               if (err) throw err;
               console.log(`Se han eliminado 3 warns medios de ${user.username}.`);
             });
         } else if (warnLevel === 'grave' && warnCount.grave >= 3) {
             applyWarn = false;
-            banMessage = `⚠ ${user.username} debe de ser baneado por superar la cantidad máxima de warns graves. (*Todos sus datos ya han sido eliminados*)`;
+            banMessage = emojis.warn` | ${user.username} debe de ser baneado por superar la cantidad máxima de warns graves. (*Todos sus datos ya han sido eliminados*)`;
             db.query(`DELETE FROM warns WHERE user_id = '${user.id}'`, (err) => {
                 if (err) throw err;
                 console.log(`Se han eliminado todos los warns de ${user.username}.`);
@@ -111,9 +109,9 @@ if (!message.member.permissions.has('MANAGE_ROLES')) {
             db.query(`INSERT INTO warns (user_id, reason, level, staff) VALUES ('${user.id}', '${reason}', '${warnLevel}', '${message.author.id}')`, (err) => {
               if (err) {
                 console.error(err);
-                return message.reply('¡Ups! Ha ocurrido un error al guardar el aviso en la base de datos.');
+                return message.reply('Me cachis! Ha ocurrido un error al guardar el warn en la base de datos.');
               }
-              message.channel.send(`${user.tag} ha recibido un warn ${warnLevel} con la razón ${reason}`);
+              message.channel.send(`**${user.tag}** ha recibido un warn **${warnLevel}** con la razón: \`${reason}\``);
             });
           } else {
             // Si no se aplica el warn, se envía un mensaje de advertencia y se sugiere el baneo
@@ -122,12 +120,11 @@ if (!message.member.permissions.has('MANAGE_ROLES')) {
         })        
   } else if (message.content.startsWith('lt!historial')) {
       	      if (!message.member.permissions.has('MANAGE_ROLES')) {
- 		 return message.reply('No tienes permiso para ejecutar este comando');
+ 		            return;
               }
     const args = message.content.trim().split(/ +/g);
     const user = message.mentions.users.first();
     const user_id = user ? user.id : args[1];
-    console.log(`user_id: ${user_id}`);
 
     if (!user_id) {
       return message.reply('Debes proporcionar la ID de un usuario.');
@@ -136,18 +133,18 @@ if (!message.member.permissions.has('MANAGE_ROLES')) {
     db.query(`SELECT * FROM warns WHERE user_id = '${user_id}' ORDER BY timestamp DESC`, (err, results) => {
         if (err) {
           console.log(err);
-          return message.channel.send('Error al obtener los warns.');
+          return message.channel.send(':x: | Ha ocurrido un error al obtener los warns del usuario (BACKEND-CONNECTION-TIMEOUT). Pruébalo más tarde.');
         }
       
         if (results.length === 0) {
-            message.channel.send(`${user} no tiene warns.`);
+            message.channel.send(`:x: | El usuario **${user.tag}** no tiene ningún registro vigente en su historial.`);
           } else {
             let warnList = "";
             for (let i = 0; i < results.length; i++) {
               const warn = results[i];
-              warnList += `${i + 1}. **Staff:** ${warn.staff} **Motivo:** ${warn.reason} **Nivel:** ${warn.level} **Fecha:** ${warn.timestamp}\n`;
+              warnList += `${i + 1}. **Staff:** <@${warn.staff}>(${warn.staff}) **Razón:** ${warn.reason} **Gravedad:** ${warn.level} **Insinuado el:** ${warn.timestamp}\n`;
             }
-            message.channel.send(`Lista de warns para  ${user}:\n${warnList}`);
+            message.channel.send(`Lista de warns para  **${user.tag}**:\n${warnList}`);
           }
           
       
